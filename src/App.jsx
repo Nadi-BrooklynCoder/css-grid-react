b4eaad591e882532394072d728f26c7fdd26d261
nmatos <nmatos@pursuit.org>
2024-04-12 18:26:32 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Grid from './Components/grid/Grid'
+import './App.css'
+
+function App() {
+return (
+  <>
+<Grid />
+  </>
+  )
+}
+
+export default App
---
+++ src/Components/grid/Grid.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './grid.css'
+
+export default function Grid () {
+  return (
+    <div className='container'>
+        <div className="container__white one">1</div>
+        <div className="container__white two">2</div>
+        <div className="container__white three">3</div>
+        <div className="container__white four">4</div>
+        <div className="container__white five">5</div>
+        <div className="container__white six">6</div>
+        <div className="container__white seven">7</div>
+        <div className="container__white eight">8</div>
+        <div className="container__white nine">9</div>
+        <div className="container__white ten">10</div>
+        <div className="container__white eleven">11</div>
+        <div className="container__white twelve">12</div>
+        <div className="container__white thirteen">13</div>
+        <div className="container__white fourteen">14</div>
+        <div className="container__white fifteen">15</div>
+        <div className="container__white sixteen">16</div>
+        <div className="container__blue seventeen">17</div>
+        <div className="container__blue eighteen">18</div>
+        <div className="container__blue nineteen">19</div>
+        <div className="container__blue twenty">20</div>
+        <div className="container__black twenty-one">21</div>
+        <div className="container__black twenty-two">22</div>
+        <div className="container__black twenty-three">23</div>
+        <div className="container__black twenty-four">24</div>
+        <div className="container__red twenty-five">25</div>
+        <div className="container__red twenty-six">26</div>
+        <div className="container__red twenty-seven">27</div>
+        <div className="container__red twenty-eight">28</div>
+        <div className="container__red twenty-nine">29</div>
+        <div className="container__yellow thirty">30</div>
+        <div className="container__yellow thirty-one">31</div>
+        <div className="container__yellow thirty-two">32</div>
+        <div className="container__yellow thirty-three">33</div>
+        <div className="container__yellow thirty-four">34</div> 
+    </div>
+  )
+}
